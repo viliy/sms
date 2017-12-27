79dbb88ac63b494e32239e9f6830d173a5115bf1
qxy <qiuxiaoyong@linghit.com>
2017-12-27 17:17:08 +0800

    feat: init

---
+++ src/FastDSMS.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiuxiaoyong
+ * Date: 2017/12/27
+ * Time: 下午5:13
+ */
+namespace Viliy\FastDSMS;
+
+/**
+ * Class FastDSMS
+ * @package Viliy\FastDSMS
+ */
+class FastDSMS
+{
+
+}
